642e902f95c234afd94c3113de38c47bf64ba83b
tnhnkzc <tunahankuzucuu@gmail.com>
2023-04-22 17:06:42 +0200

    Initial commit

---
+++ src/server/api/root.ts
@@ -0,0 +1,13 @@
+import { createTRPCRouter } from "~/server/api/trpc";
+import { notesRouter } from "~/server/api/routers/notes";
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  posts: notesRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
